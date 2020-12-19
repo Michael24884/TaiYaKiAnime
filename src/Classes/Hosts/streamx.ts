a4746dfb09151c16173f90483a617cf8d2c70122
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2020-12-19 18:21:32 +0200

    Create streamx.ts

---
+++ src/Classes/Hosts/streamx.ts
@@ -0,0 +1,28 @@
+import {EmbededResolvedModel} from '../../Models/taiyaki';
+import { unPack } from "./unpacker";
+import { HostBase } from "./base";
+
+export default class StreamX extends HostBase {
+	grabAvailableHosts = async (
+		embedLink: string
+	): Promise<EmbededResolvedModel[]> => {
+		const init = {
+			method: 'GET',
+			headers: {"referer": "https://kisscartoon.nz/"}
+		}
+		const html = await (await fetch(embedLink, init)).text();
+		const matchGroup = html.match(/sources\: \[([^]*?)\]/m);
+		if (matchGroup) {
+			if (matchGroup.length >= 2) {
+				const sources = JSON.parse('[' + matchGroup[1] + ']').map((source) => {
+					return {
+						quality: source["label"],
+						link: source["file"],
+					};
+				});
+			} else {
+				console.log("throwing");
+				throw new Error("Error 404: File not found")
+			};
+	};
+};
